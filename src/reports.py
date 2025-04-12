654ddc99b0f30981630a56dceed2e6f95a5e9f37
Sergey <gaufss@gmail.com>
2025-04-12 21:35:28 +0500

    Доработка согласно замечаний

--- src/reports.py
+++ src/reports.py
@@ -5,8 +5,6 @@ from typing import Optional
 
 import pandas as pd
 
-from src.utils import PATH_TO_FILE
-
 # Настройка логирования
 logging.basicConfig(
     filename="../logs/reports.log",
@@ -25,57 +23,54 @@ def report_decorator(filename=None):
 
     def decorator(func):
         @wraps(func)
-        def wrapper(filename=None, *args, **kwargs):
+        def wrapper(*args, **kwargs):
             # Вызов функции для получения отчета
             result = func(*args, **kwargs)
-
             # Определяем имя файла
-            if filename is None:
-                filename = f"report_{datetime.now().strftime('%Y%m%d_%H%M%S')}.txt"
-                # filename = f"report_{func.__name__}_{datetime.now().strftime('%Y%m%d_%H%M%S')}.txt"
-
+            report_filename = filename or f"report_{func.__name__}_{datetime.now().strftime('%Y%m%d_%H%M%S')}.txt"
             # Запись результата в файл
-            with open(filename, "w", encoding="utf-8") as file:
-                file.write(str(result))
-
-            logging.INFO(f"Отчет сохранен в {filename}")
+            try:
+                with open(report_filename, "w", encoding="utf-8") as file:
+                    file.write(str(result))
+                logger.info(f"Отчет сохранен в {report_filename}")
+            except Exception as e:
+                logger.error(f"Ошибка при сохранении отчета: {e}")
             return result
 
         return wrapper
 
     return decorator
-    # return decorator if filename is not None else decorator(None)
 
 
 # Пример использования декоратора
 @report_decorator(filename="mylog.txt")
-def spending_by_category(transactions: pd.DataFrame, category: str, date: Optional[str] = None) -> pd.DataFrame:
+def spending_by_category(transactions: pd.DataFrame, category: str, date: Optional[str] = None) -> float:
     # Если дата не передана, используем текущую дату
     if date is None:
         date = datetime.now()
-        logging.INFO("Дата не указана, используется текущая дата: %s", date)
+        logger.info("Дата не указана, используется текущая дата: %s", date)
     else:
         # Преобразуем дату в формат datetime
-        date = pd.to_datetime(date)
-        logging.INFO("Использование предоставленной даты: %s", date)
+        date = pd.to_datetime(date, dayfirst=True)
+        logger.info("Использование предоставленной даты: %s", date)
 
     # Определяем дату три месяца назад
     three_months_ago = date - pd.DateOffset(months=3)
 
+    transactions["Дата операции"] = pd.to_datetime(
+        transactions["Дата операции"],
+        format="%d.%m.%Y %H:%M:%S"
+    )
+
     # Фильтруем дата фрейм по дате и категории
     filtered_df = transactions[
-        (transactions["Дата платежа"] >= three_months_ago)
-        & (transactions["Дата платежа"] <= date)
+        (transactions["Дата операции"] >= three_months_ago)
+        & (transactions["Дата операции"] <= date)
         & (transactions["Категория"] == category)
-    ]
+        ]
 
     # Суммируем траты по заданной категории
-    total_expenses = filtered_df["Сумма платежа"].sum()
-    logging.INFO(f"Сформированный отчет для категории: {category}, общий расход: {total_expenses}")
+    total_expenses = filtered_df["Сумма операции"].sum()
+    logger.info(f"Сформированный отчет для категории: {category}, общий расход: {total_expenses}")
 
     return total_expenses
-
-
-# transactions = pd.read_excel(PATH_TO_FILE)
-# transactions_df = pd.DataFrame(transactions)
-# print(spending_by_category(transactions_df, "Супермаркеты", "31.12.2021"))
